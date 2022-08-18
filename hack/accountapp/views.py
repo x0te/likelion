b6dc6cc9623365eccb830853497bef5abaeefa41
x0te <jh07151@gmail.com>
2022-08-19 08:05:35 +0900

    쥰내피곤

--- hack/accountapp/views.py
+++ hack/accountapp/views.py
@@ -1,9 +1,36 @@
-from http.client import HTTPResponse
+#from http.client import HTTPResponse
+#from msilib.schema import Class
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+
 # Create your views here.
+from accountapp.models import HelloWorld
 
 def hello_world(request):
     #return HttpResponse('Hello world!')
-    return render(request, 'accountapp/helloworld.html')
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url: reverse_lazy('accountapp:hello_world')
+    template_name= 'accountapp/create.html'
 
