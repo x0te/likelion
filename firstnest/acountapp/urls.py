7877f1bfa0327e618bda4a9e3dde10bb557c724f
x0te <jh07151@gmail.com>
2022-08-19 00:02:55 +0900

    last

--- firstnest/acountapp/urls.py
+++ firstnest/acountapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from acountapp.views import hello_world
 
-urlpatterns = {
-    path
-}
+app_name = "accountapp"
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello world')
+]
+    
